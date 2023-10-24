410354f9af1527a840e77c5538c9e301bb3ba0bc
Altfoxy <97593444+Altfoxy@users.noreply.github.com>
2023-10-24 11:52:46 +0300

    Create Port_check.sh

---
+++ Port_check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Проверка существования файла
+if [ ! -f /usr/local/bin/servers ]; then
+    echo "Файл /root/servers не найден."
+    exit 1
+fi
+
+# Чтение файла и проверка доступности порта
+while IFS=: read -r ip port name; do
+    result=$(nmap -p "$port" -T4 "$ip" | grep -o "open" || echo "closed")
+    
+    if [ "$result" = "open" ]; then
+        status="1"
+    else
+        status="0"
+    fi
+
+    echo "availability,server=$ip,port=$port,nameserv=$name status=$status"
+done < /usr/local/bin/servers
